541a501caea308dbc47b71d13913ee68c324c926
Alex <alexander.schlueter86@gmx.de>
2025-06-02 10:26:43 +0200

    02.06.2025 Cluster ebody whole script

--- pygalmesh/data/scripts/001-Special-Issue-2025/02_create_folders.sh
+++ pygalmesh/data/scripts/001-Special-Issue-2025/02_create_folders.sh
@@ -4,16 +4,17 @@
 set -e
 
 # Define source and destination directories
-SRC_DIR="$HOME/meshing/Meshing/pygalmesh"
-DEST_DIR="$HPC_SCRATCH/pygalmesh"
+SRC_DIR="$HOME/meshing/Meshing/pygalmesh/"
+DEST_DIR="$HPC_SCRATCH/pygalmesh/"
 
 # Create destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 
-# Copy directory and contents
-cp -r "$SRC_DIR"/* "$DEST_DIR"
+# Sync files: only copy new/updated files, keep existing ones, don't delete anything
+rsync -av --no-delete "$SRC_DIR" "$DEST_DIR"
 
-echo "Contents copied from $SRC_DIR to $DEST_DIR"
+echo "Folder structure and updated files copied from $SRC_DIR to $DEST_DIR"
+
 
 
 
